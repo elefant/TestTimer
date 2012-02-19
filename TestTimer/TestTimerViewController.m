295fb4630a6174ca210a3057ddf1fea833393387
bird <holin20@gmail.com>
2012-02-19 17:14:30 +0800

    Complete the function I want to test in this project
    1) How to start a timer
    2) How to stop a timer
    3) Instance variable default valie

--- TestTimer/TestTimerViewController.m
+++ TestTimer/TestTimerViewController.m
@@ -8,5 +8,76 @@
 
 #import "TestTimerViewController.h"
 
+@interface TestTimerViewController()
+
+//
+// UI outlets
+//
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+
+//
+// Properties
+//
+@property (strong, nonatomic) NSTimer* myTimer; 
+
+@property (nonatomic) int count; 
+
+//
+// Methods
+//
+-(void)timerFireMethod:(NSTimer*)aTimer;
+
+-(void)updateTimerLabel;
+
+@end
+
 @implementation TestTimerViewController
+
+//
+// synthesize
+//
+@synthesize timerLabel = mTimerLabel;
+
+@synthesize myTimer = mMyTimer;
+
+@synthesize count = mCount; // will be set to 0 even if we don't init it in the constructor 
+
+//
+// Method implementation
+//
+- (IBAction)onTimerStartClicked:(UIButton*)sender 
+{
+    self.myTimer = 
+        [NSTimer scheduledTimerWithTimeInterval:0.5
+                                         target:self 
+                                       selector:@selector(timerFireMethod:)
+                                       userInfo:nil 
+                                        repeats:YES];
+}
+
+- (IBAction)onTimerStopClicked:(id)sender 
+{
+    [self.myTimer invalidate];
+    self.myTimer = nil;
+    self.count = 0;
+    [self updateTimerLabel];
+}
+
+- (void)timerFireMethod:(NSTimer*)aTimer
+{
+    NSLog( @"test" );
+    self.count++;
+    [self updateTimerLabel];
+}
+
+-(void)updateTimerLabel
+{
+    self.timerLabel.text = [NSString stringWithFormat:@"%d", self.count];
+}
+
+- (void)viewDidUnload 
+{
+    [self setTimerLabel:nil];
+    [super viewDidUnload];
+}
 @end
